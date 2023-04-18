42b47a9ea43bb207c8e664939cfc5f5deb407d71
iamgaziev <iamgaziev@mail.ru>
2023-04-18 17:03:35 +0500

    practice

---
+++ main.js
@@ -0,0 +1,132 @@
+// task 1
+
+// function method(n){
+//     let m = "Something"
+//     let x = m.concat(" ",n)
+//     return x
+// }
+// console.log(method("Bob Jane"));
+
+// task 2
+
+// function method(n){
+//     let m = "edabit"
+//     let x = n.concat(m)
+//     return x
+// }
+// console.log(method("Javascript"));
+
+
+// task 3
+
+// function method(str){
+//     let mas = str.split("")
+//     let cnt = 0
+//     for(let i = 0; i<=mas.length; i++){
+//         if(str[i]=="a" || str[i]=="A"){
+//             cnt++
+//         }
+//         else if(str[i]=="o" || str[i]=="O"){
+//             cnt++
+//         }
+//         else if(str[i]=="i" || str[i]=="I"){
+//             cnt++
+//         }
+//         else if(str[i]=="e" || str[i]=="E"){
+//             cnt++
+//         }
+//         else if(str[i]=="u" || str[i]=="U"){
+//             cnt++
+//         }
+//         else if(str[i]=="y" || str[i]=="Y"){
+//             cnt++
+//         }
+//     }
+//     return cnt
+// }
+// console.log(method("celebration"));
+
+
+// task 5
+
+// function method(str){
+//     let x = str.split(" ")
+//     // console.log(x);
+//     for(let i = 0; i<=x.length; i++){
+//         if(x[i]=="bomb"){
+//             return "Duck!!!"
+//         }
+//     }
+//     return "There is no bomb, relax"
+// }
+// console.log(method("this goes bomb"));
+
+// task 6 
+
+// let cnt = 0
+// function charCount(a,b){
+//     let x = b.split("")
+//     for(let i = 0; i < x.length; i++){
+//         if(a == x[i]){
+//             cnt ++
+//         }
+//     }
+//     return cnt
+// }
+// console.log(charCount('a', 'edabit'));
+
+
+// task  4
+
+// function method(str){
+//     let mas = str.split("")
+//     for(let i = 0; i<=mas.length; i++){
+//         if(str[i]=="a" || str[i]=="A"){
+//             return str.indexOf("a")
+//         }
+//         else if(str[i]=="o" || str[i]=="O"){
+//             return str.indexOf("o")
+//         }
+//         else if(str[i]=="i" || str[i]=="I"){
+//             return str.indexOf("i")
+//         }
+//         else if(str[i]=="e" || str[i]=="E"){
+//             return str.indexOf("e")
+//         }
+//         else if(str[i]=="u" || str[i]=="U"){
+//             return str.indexOf("u")
+//         }
+//         else if(str[i]=="y" || str[i]=="Y"){
+//             return str.indexOf("y")
+//         }
+//     }
+  
+// }
+// console.log(method("hello"));
+
+// task 7
+
+// let n = ""
+// function method(str){
+//     let x = str.split("")
+//     // console.log(x);
+//     for(let i=0; i<x.length; i++){
+//         n+=x[i].repeat(2)
+//     }
+//     return n
+// }
+// console.log(method("string"));
+
+
+// task 8
+// function sayHello(a,b){
+//     if(b==0){
+//         b= "bye"
+//     }
+//     else{
+//         b= "Hello"
+//     }
+//     return b.concat(" ",a)
+// }
+// console.log(sayHello("alon",0));
+
